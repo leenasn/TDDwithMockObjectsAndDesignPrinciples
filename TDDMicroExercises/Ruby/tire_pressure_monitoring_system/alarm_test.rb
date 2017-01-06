f947513aeb5ded01f6e996e343fb30137eab62e2
Leena <leenasn@gmail.com>
2017-01-06 11:03:43 +0530

    Add tests for Alarm check. Refactoring is pending

--- TDDMicroExercises/Ruby/tire_pressure_monitoring_system/alarm_test.rb
+++ TDDMicroExercises/Ruby/tire_pressure_monitoring_system/alarm_test.rb
@@ -1,9 +1,24 @@
 require 'test/unit'
+require 'mocha/test_unit'
 require_relative './alarm'
 
 class AlarmTest < Test::Unit::TestCase
-  def test_do_something
+  def test_alarm_is_off_with_right_pressure
     alarm = Alarm.new
+    Sensor.any_instance.stubs(:pop_next_pressure_psi_value).returns(20)
     alarm.check
+    assert(alarm.alarm_on == false)
+  end
+  def test_alarm_is_on_when_below_pressure
+    alarm = Alarm.new
+    Sensor.any_instance.stubs(:pop_next_pressure_psi_value).returns(16)
+    alarm.check
+    assert(alarm.alarm_on == true)
+  end
+  def test_alarm_is_on_when_above_pressure
+    alarm = Alarm.new
+    Sensor.any_instance.stubs(:pop_next_pressure_psi_value).returns(22)
+    alarm.check
+    assert(alarm.alarm_on == true)
   end
 end
