6947e489cf2ad376153d55fc97cd1a056ebc20e7
Leena <leenasn@gmail.com>
2017-01-09 12:27:43 +0530

    Test for telemetry system

--- TDDMicroExercises/Ruby/telemetry_system/telemetry_diagnostic_controls_test.rb
+++ TDDMicroExercises/Ruby/telemetry_system/telemetry_diagnostic_controls_test.rb
@@ -1,12 +1,24 @@
 require 'test/unit'
 require_relative './telemetry_diagnostic_controls'
+require 'mocha/test_unit'
 
 class TelemetryDiagnosticControlsTest < Test::Unit::TestCase
 
-  def test_do_something
+ def test_print_diagnostic_info_if_connected
+    TelemetryClient.any_instance.stubs(:online_status).returns(true)
     ctrl = TelemetryDiagnosticControls.new
-
+    TelemetryClient.any_instance.expects(:send)
+    TelemetryClient.any_instance.expects(:receive)
     ctrl.check_transmission
-    ctrl.diagnostic_info
+    assert_not_equal('',ctrl.diagnostic_info)
+  end
+
+ def test_raise_error_if_not_connected
+    TelemetryClient.any_instance.stubs(:online_status).returns(false)
+    ctrl = TelemetryDiagnosticControls.new
+    assert_raise do
+      ctrl.check_transmission
+    end
+    assert_equal('',ctrl.diagnostic_info)
   end
 end
